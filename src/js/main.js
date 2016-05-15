b555c7018cd150e8a738ba39657259a26e8da20c
danielmoi <daniel@ampersandmoi.com>
2016-05-16 09:35:02 +1000

    basic implementation of routes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 "use strict";
 
+var _ReactRouter = ReactRouter;
+var Router = _ReactRouter.Router;
+var Route = _ReactRouter.Route;
+var IndexRoute = _ReactRouter.IndexRoute;
+var Link = _ReactRouter.Link;
+var browserHistory = _ReactRouter.browserHistory;
+
+
 var Header = React.createClass({
   displayName: "Header",
 
@@ -20,8 +28,8 @@ var Header = React.createClass({
   }
 });
 
-var Main = React.createClass({
-  displayName: "Main",
+var Projects = React.createClass({
+  displayName: "Projects",
 
   render: function render() {
     return React.createElement(
@@ -35,7 +43,7 @@ var Main = React.createClass({
       React.createElement(
         "p",
         null,
-        "Project 0: ",
+        "Project 0:",
         React.createElement(
           "a",
           { href: "/" },
@@ -45,7 +53,7 @@ var Main = React.createClass({
       React.createElement(
         "p",
         null,
-        "Project 0: ",
+        "Project 0:",
         React.createElement(
           "a",
           { href: "/" },
@@ -55,7 +63,7 @@ var Main = React.createClass({
       React.createElement(
         "p",
         null,
-        "Project 1: ",
+        "Project 1:",
         React.createElement(
           "a",
           { href: "/" },
@@ -65,7 +73,7 @@ var Main = React.createClass({
       React.createElement(
         "p",
         null,
-        "Project 2: ",
+        "Project 2:",
         React.createElement(
           "a",
           { href: "/" },
@@ -75,7 +83,7 @@ var Main = React.createClass({
       React.createElement(
         "p",
         null,
-        "Project 3: ",
+        "Project 3:",
         React.createElement(
           "a",
           { href: "/" },
@@ -85,6 +93,33 @@ var Main = React.createClass({
     );
   }
 });
+
+var Skills = React.createClass({
+  displayName: "Skills",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "main__container" },
+      React.createElement(
+        "h2",
+        { className: "main__title" },
+        "Skills"
+      ),
+      React.createElement(
+        "p",
+        null,
+        "HTML5"
+      ),
+      React.createElement(
+        "p",
+        null,
+        "JavaScript"
+      )
+    );
+  }
+});
+
 var Footer = React.createClass({
   displayName: "Footer",
 
@@ -115,11 +150,85 @@ var App = React.createClass({
       "div",
       null,
       React.createElement(Header, null),
-      React.createElement(Main, null),
       React.createElement(Footer, null)
     );
   }
 });
 
 // This is the highest level one
-ReactDOM.render(React.createElement(App, null), document.querySelector('#app'));
+// ReactDOM.render(
+//   <App/>, document.querySelector('#app'));
+var Home = React.createClass({
+  displayName: "Home",
+
+  render: function render() {
+    return React.createElement(
+      "h1",
+      null,
+      "Welcome to the Home Page"
+    );
+  }
+});
+var MainLayout = React.createClass({
+  displayName: "MainLayout",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "app" },
+      React.createElement("header", { className: "primary-header" }),
+      React.createElement(
+        "div",
+        { className: "primary-aside" },
+        React.createElement(
+          "ul",
+          null,
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              Link,
+              { to: "/" },
+              "Home"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              Link,
+              { to: "/skills" },
+              "Skills"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              Link,
+              { to: "/projects" },
+              "Projects"
+            )
+          )
+        )
+      ),
+      React.createElement(
+        "main",
+        null,
+        this.props.children
+      )
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(
+  Router,
+  null,
+  React.createElement(
+    Route,
+    { path: "/", component: MainLayout },
+    React.createElement(IndexRoute, { component: Home }),
+    React.createElement(Route, { path: "/skills", component: Skills }),
+    React.createElement(Route, { path: "/projects", component: Projects })
+  )
+), document.getElementById('app'));
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,3 +1,5 @@
+var {Router, Route, IndexRoute, Link, browserHistory} = ReactRouter
+
 var Header = React.createClass({
   render: function() {
     return (
@@ -10,20 +12,44 @@ var Header = React.createClass({
   }
 });
 
-var Main = React.createClass({
+var Projects = React.createClass({
   render: function() {
     return (
       <main className="main__container">
         <h2 className="main__title">Projects</h2>
-        <p>Project 0: <a href="/">Tic Tac Toe</a></p>
-        <p>Project 0: <a href="/">Catbus 4000</a></p>
-        <p>Project 1: <a href="/">Sortr</a></p>
-        <p>Project 2: <a href="/">BuddyUP</a></p>
-        <p>Project 3: <a href="/">Musical Monsters</a></p>
+        <p>Project 0:
+          <a href="/">Tic Tac Toe</a>
+        </p>
+        <p>Project 0:
+          <a href="/">Catbus 4000</a>
+        </p>
+        <p>Project 1:
+          <a href="/">Sortr</a>
+        </p>
+        <p>Project 2:
+          <a href="/">BuddyUP</a>
+        </p>
+        <p>Project 3:
+          <a href="/">Musical Monsters</a>
+        </p>
       </main>
     )
   }
-})
+});
+
+var Skills = React.createClass({
+  render: function() {
+    return (
+      <div className="main__container">
+        <h2 className="main__title">Skills</h2>
+        <p>HTML5</p>
+        <p>JavaScript</p>
+      </div>
+    )
+  }
+});
+
+
 var Footer = React.createClass({
   render: function() {
     return (
@@ -42,14 +68,50 @@ var App = React.createClass({
 
     return (
       <div>
-        <Header />
-        <Main />
-        <Footer />
+        <Header/>
+        <Footer/>
       </div>
     );
   }
 });
 
 // This is the highest level one
-ReactDOM.render(<App  />,
-             document.querySelector('#app'));
+// ReactDOM.render(
+//   <App/>, document.querySelector('#app'));
+var Home = React.createClass({
+  render: function() {
+    return (
+      <h1>Welcome to the Home Page</h1>
+    );
+  }
+});
+var MainLayout = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        <header className="primary-header"></header>
+        <div className="primary-aside">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/skills">Skills</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+          </ul>
+        </div>
+        <main>
+          {this.props.children}
+        </main>
+      </div>
+      )
+  }
+})
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={MainLayout}>
+      <IndexRoute component={Home} />
+      <Route path="/skills" component={Skills} />
+      <Route path="/projects" component={Projects} />
+
+    </Route>
+  </Router>
+), document.getElementById('app'));
