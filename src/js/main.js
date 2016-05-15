b0476620d07e1d07aa4df834ebbf2c03484956b8
danielmoi <daniel@ampersandmoi.com>
2016-05-15 11:18:10 +1000

    Add header element with h1 with React

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
-'use strict';
+"use strict";
 
-console.log('from main.jsx with browserSync');
+var Header = React.createClass({
+  displayName: "Header",
+
+  render: function render() {
+    return React.createElement(
+      "h1",
+      { className: "header__title" },
+      "Daniel Moi"
+    );
+  }
+});
+
+var App = React.createClass({
+  displayName: "App",
+
+
+  render: function render() {
+
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(Header, null)
+    );
+  }
+});
+
+// This is the highest level one
+ReactDOM.render(React.createElement(App, null), document.querySelector('#app'));
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,1 +1,23 @@
-console.log('from main.jsx with browserSync');
+var Header = React.createClass({
+  render: function() {
+    return (
+      <h1 className="header__title">Daniel Moi</h1>
+    )
+  }
+});
+
+var App = React.createClass({
+
+  render: function() {
+
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+});
+
+// This is the highest level one
+ReactDOM.render(<App  />,
+             document.querySelector('#app'));
